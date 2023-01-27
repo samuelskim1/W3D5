4730cb7b760d8fa26c3ad21f98857e84393a557c
Kaiter Wu <wukaiter@gmail.com>
2023-01-27 14:17:27 -0500

    adding getter methods and defining parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,16 +1,17 @@
 class PolyTreeNode
 
-    attr_reader :value
-
+    attr_reader :value, :children, :parent
     def initialize(value)
         @value = value
         @children = []
-        @parent
+        @parent = nil 
     end
 
-    def parent=(parent)
-        @parent = nil
-    end
-
+    def parent=(par)
+        unless par == nil
+            @parent = par 
+            par.children << self if !par.children.include?(self) 
+        end 
+    end 
 
 end
