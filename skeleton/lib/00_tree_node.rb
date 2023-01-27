5658ca7854514207b5bdd59b74e92438134b8dce
Kaiter Wu <wukaiter@gmail.com>
2023-01-27 14:54:35 -0500

    working on parent = method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -8,13 +8,15 @@ class PolyTreeNode
     end
 
     def parent=(new_parent)
-        unless new_parent == nil
-            @parent = new_parent
+        if new_parent != nil
             old_parent = self.parent
             old_parent_children = old_parent.children
             old_parent_children.delete(self) 
             #come back and check what we're passing into @children
-            new_parent.children << self if !new_parent.children.include?(self) 
+           new_parent.children << self if !new_parent.children.include?(self) 
+           @parent = new_parent 
+        else 
+             @parent = nil 
         end 
        
     end 
