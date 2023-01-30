4a5d3679007b2e7486d9bf2f9cc30d76baa3efc0
Kaiter Wu <wukaiter@gmail.com>
2023-01-30 10:51:49 -0500

    finishing self.valid_moves

--- knight_travails/knight_path_finder.rb
+++ knight_travails/knight_path_finder.rb
@@ -10,8 +10,25 @@ class KnightPathFinder
     end 
 
     def self.valid_moves(pos)
+        combos = [] 
        x = [-2, -1, 1, 2]
        y = [-2, -1, 1, 2]
+
+       x.each_with_index do |el1,i1| 
+        y.each_with_index do |el2,i2| 
+           combos << [el1,el2] if i2>i1 
+        end 
+        end 
+        poss_combs = [] 
+        combos.each do |position| 
+            new_pos =[]
+            new_pos<< (position.first + pos.first) 
+            new_pos <<(position.last + pos.last)
+            
+           poss_combs << new_pos if new_pos.all?{|index|index>=0 && index < 8 }
+              
+        end 
+        poss_combs 
     end 
 
     def new_move_positions(pos)
