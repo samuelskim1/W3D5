3b62d0d44dc4248968db301c2bf9a70d7baadc2f
Kaiter Wu <wukaiter@gmail.com>
2023-01-30 17:18:05 -0500

    finishing children method

--- TTT/lib/tic_tac_toe_node.rb
+++ TTT/lib/tic_tac_toe_node.rb
@@ -1,7 +1,15 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board 
+    @next_mover_mark = next_mover_mark 
+
+
+    @prev_move_pos = prev_move_pos
+    
   end
 
   def losing_node?(evaluator)
@@ -10,8 +18,27 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
+  #This method generates an array of all moves that can be made after
+  #the current move.
   def children
+    childrens = [] 
+    @board.rows.each_with_index do |row,i|
+      row.each_with_index do |ele,j| 
+        if @board.rows[i][j].nil? 
+          other_mark = nil 
+          child_board = @board.dup 
+          child_board.rows[i][j] = self.next_mover_mark
+          if self.next_mover_mark == :x 
+            other_mark = :o 
+          else   
+            other_mark = :x 
+          end  
+          new_previous_pos = [i,j]
+          child = TicTacToeNode.new(child_board,other_mark,new_previous_pos )
+          childrens << child 
+        end 
+      end 
+    end 
+    childrens 
   end
-end
+end 
